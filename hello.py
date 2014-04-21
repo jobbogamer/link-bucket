911c10e633dbf1b2a4271b2b9742df091773c6b3
Josh Asch <josh.asch@gmail.com>
2014-04-21 21:10:25 +0100

    Add stuff for database

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 @app.route('/')
 def hello():
 	return 'Hello World!'
+
+if (__name__ == "__main__"):
+	app.run()
