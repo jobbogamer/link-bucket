0b72e5becd724a44bde1032b65c2348ff8298069
Josh Asch <josh.asch@gmail.com>
2014-04-22 12:02:30 +0100

    Create database model

--- link-bucket.py
+++ link-bucket.py
@@ -9,6 +9,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost:5432"
 
 db = SQLAlchemy(app)
 
+
+class Link(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	url = db.Column(db.String(512))
+	date = db.Column(db.DateTime)
+	archived = db.Column(db.Boolean)
+	title = db.Column(db.String)
+	icon = db.Column(db.String)
+
+	def __init__(self, url, date, title, icon):
+		self.url = url
+		self.date = date
+		self.archived = false
+		self.title = title
+		self.icon = icon
+
+	def __repr__(self):
+		return "<Link(" + str(self.id) + ") " + str(self.url) + " >"
+
+
 @app.route('/')
 def hello():
 	return 'Hello World!'
