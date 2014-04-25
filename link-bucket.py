74ee74f4ba9e5f79dd4934aaa66a8b941cde8952
Josh Asch <josh.asch@gmail.com>
2014-04-25 22:26:08 +0100

    Relative dates and ages as CSS classes

--- link-bucket.py
+++ link-bucket.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, flash, url_for, abort, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -8,6 +9,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost:5432"
 
 db = SQLAlchemy(app)
+
+
+def get_relative_time(seconds):
+	if seconds < 60:
+		return "<1m"
+	elif seconds < (60 * 60):
+		return str(int(seconds / 60)) + "m"
+	elif seconds < (24 * 60 * 60):
+		return str(int(seconds / (60 * 60))) + "h"
+	elif seconds < (7 * 24 * 60 * 60):
+		return str(int(seconds / (24 * 60 * 60))) + "d"
+	elif seconds < (365 * 24 * 60 * 60):
+		return str(int(seconds / (7 * 24 * 60 * 60))) + "w"
+	else:
+		return str(int(seconds / (365 * 24 * 60 * 60))) + "y"
 
 
 class Link(db.Model):
@@ -32,7 +48,27 @@ class Link(db.Model):
 @app.route('/')
 def index():
 	items = Link.query.filter_by(archived = False).all()
-	return render_template('index.html', items=items)
+	now = datetime.now()
+
+	ages = {}
+	times = {}
+	for item in items:
+		delta = now - item.date
+
+		seconds = delta.total_seconds()
+		times[item.id] = get_relative_time(seconds)
+
+		days = delta.days
+		if days < 7:
+			ages[item.id] = "days"
+		elif days < 28:
+			ages[item.id] = "weeks"
+		elif days < 365:
+			ages[item.id] = "months"
+		else:
+			ages[item.id] = "years"
+
+	return render_template('index.html', items=items, ages=ages, times=times)
 
 if (__name__ == "__main__"):
 	app.run(debug = True)
