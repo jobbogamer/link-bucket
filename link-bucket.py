7917905541b2ac0debf3b2fb03e9d266d2e3370d
Josh Asch <josh.asch@gmail.com>
2014-04-28 23:29:39 +0100

    MASSIVE COMMIT adding automatic link scraping from ffacebook

--- link-bucket.py
+++ link-bucket.py
@@ -1,7 +1,11 @@
 import os
-from datetime import datetime
+import facebook
+import re
+from urllib2 import urlopen
+from pprint import pformat
+from datetime import datetime, timedelta
 from urlparse import urlparse
-from flask import Flask, render_template, flash, url_for, abort, request, jsonify
+from flask import Flask, render_template, flash, url_for, abort, request, jsonify, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost:5432"
 
 db = SQLAlchemy(app)
 
+base_url = 'http://localhost:5000'
 
 def get_relative_time(seconds):
 	if seconds < 60:
@@ -44,6 +49,123 @@ class Link(db.Model):
 	def __repr__(self):
 		return "[Link(%d) %s]" % (self.id, self.url)
 
+
+class FB(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	app_id = db.Column(db.String(50))
+	app_secret = db.Column(db.String(50))
+	code = db.Column(db.String(512))
+	access_token = db.Column(db.String(512))
+	expiry = db.Column(db.DateTime)
+
+	def __init__(self, app_id, app_secret):
+		self.app_id = app_id
+		self.app_secret = app_secret
+		self.code = ''
+		self.access_token = ''
+
+class Message(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	last_checked = db.Column(db.String(64))
+
+	def __init__(self, last_checked):
+		self.last_checked = last_checked
+
+def facebook_login_url():
+	details = FB.query.filter_by(id = 1).first()
+	url = facebook.auth_url(details.app_id, base_url + url_for('handle_fb'), perms=['read_mailbox'])
+	url += "&response_type=token"
+	return url
+
+def get_facebook_message_links():
+	messages = []
+
+	details = FB.query.filter_by(id = 1).first()
+	api = facebook.GraphAPI(access_token = details.access_token)
+	conversation = api.get_object('486492038125631')
+
+	last_message_time = conversation['updated_time']
+	previous_messages_url = conversation['comments']['paging']['next']
+
+	fetched_messages = conversation['comments']['data']
+	last_checked = get_last_checked_id()
+	found_last_checked = False
+
+	for message in fetched_messages:
+		if message['id'] > last_checked and message['from']['name'] == 'Josh Asch':
+			messages.append(message)
+
+		if message['id'] == last_checked:
+			found_last_checked = True
+
+	if not found_last_checked:
+		messages = find_last_checked(last_checked, previous_messages_url, messages)
+
+	if len(messages) > 0:
+		biggest_id = '0'
+		most_recent_message = None
+		for message in messages:
+			if message['id'] > biggest_id:
+				biggest_id = message['id']
+				most_recent_message = message
+
+		set_last_checked_id(biggest_id)
+
+	links = []
+
+	for message in messages:
+		text = message['message']
+		if "http" in text:
+			date = datetime.strptime(message['created_time'][0:19], '%Y-%m-%dT%H:%M:%S')
+			delta = datetime.now() - datetime.utcnow()
+			date = date + delta
+			message_link = re.search("(?P<url>https?://[^\s]+)", text).group("url")
+			link_dict = {'url': message_link, 'title': text.replace(message_link, '').strip(), 'date': date}
+			links.append(link_dict)
+
+	for link in links:
+		new_link = Link(link['url'], link['date'], link['title'])
+		db.session.add(new_link)
+
+	db.session.commit()
+
+def find_last_checked(last_checked, previous_messages_url, messages):
+	found_last_checked = False
+
+	while not found_last_checked:
+		data = eval(urlopen(previous_messages_url).read())
+		fetched_messages = data['data']
+
+		for message in fetched_messages:
+			if message['id'] >= last_checked:
+				messages.append(message)
+
+			if message['id'] == last_checked:
+				found_last_checked = True
+
+		previous_messages_url = data['paging']['next'].replace('\\', '')
+
+	return messages
+
+def get_last_checked_id():
+	details = Message.query.filter_by(id = 1).first()
+
+	if details is None:
+		set_last_checked_id('486492038125631_1398714530646')
+		return '486492038125631_1398714530646'
+
+	return details.last_checked
+
+def set_last_checked_id(id):
+	details = Message.query.filter_by(id = 1).first()
+
+	if details is None:
+		details = Message(id)
+		db.session.add(details)
+	else:
+		details.last_checked = id
+
+	db.session.commit()
 
 @app.route('/')
 def index():
@@ -144,6 +266,40 @@ def api_destroy(id):
 	db.session.delete(item)
 	db.session.commit()
 	return jsonify(success=True, error_code=0, error_msg="")
+
+
+@app.route('/fb')
+def fb():
+	return redirect(facebook_login_url())
+
+@app.route('/fb_logged_in')
+def handle_fb():
+	if 'code' in request.args:
+		details = FB.query.filter_by(id = 1).first()
+		details.code = request.args['code']
+		db.session.commit()
+		return redirect(facebook.get_access_token_from_code(details.code, base_url + url_for('handle_fb'), details.app_id, details.app_secret))
+
+	return '''  <script type="text/javascript">
+                var token = window.location.href.split("access_token=")[1];
+                window.location = "/fb_token/" + token;
+            </script> '''
+
+@app.route('/fb_token/<token>')
+def fb_token(token):
+	pos = token.find('&expires_in=')
+	expires_in = int(token[(pos + 12):])
+	delta = timedelta(seconds=expires_in)
+	expiry = datetime.now() + delta
+
+	details = FB.query.filter_by(id = 1).first()
+	details.access_token = token[:pos]
+	details.expiry = expiry
+	db.session.commit()
+
+	get_facebook_message_links()
+
+	return redirect(url_for('index'))
 
 
 if (__name__ == "__main__"):
