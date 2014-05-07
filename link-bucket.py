2f984430a0d8d30dfac735c0e6d1db2f1c893edf
Josh Asch <josh.asch@gmail.com>
2014-05-07 15:26:36 +0100

    Actually make /archive show the archive

--- link-bucket.py
+++ link-bucket.py
@@ -232,7 +232,7 @@ def add():
 
 @app.route('/archive')
 def view_archive():
-	items = Link.query.filter_by(archived = False).all()
+	items = Link.query.filter_by(archived = True).all()
 	items = sorted(items, key=lambda link: link.id, reverse=True)
 	now = datetime.now()
 
