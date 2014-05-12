8cfccc7fbdd066937f2bc080ce5fd5035c1fa556
Josh Asch <josh.asch@gmail.com>
2014-05-12 23:04:56 +0100

    Refactor all the things (closes #22)

--- link-bucket.py
+++ link-bucket.py
@@ -22,30 +22,9 @@ db = SQLAlchemy(app)
 
 base_url = 'http://linkbucket.josh-asch.net'
 
-def get_relative_time(seconds):
-	if seconds < 60:
-		return "<1m"
-	elif seconds < (60 * 60):
-		return str(int(seconds / 60)) + "m"
-	elif seconds < (24 * 60 * 60):
-		return str(int(seconds / (60 * 60))) + "h"
-	elif seconds < (7 * 24 * 60 * 60):
-		return str(int(seconds / (24 * 60 * 60))) + "d"
-	elif seconds < (365 * 24 * 60 * 60):
-		return str(int(seconds / (7 * 24 * 60 * 60))) + "w"
-	else:
-		return str(int(seconds / (365 * 24 * 60 * 60))) + "y"
-
-def get_title(url):
-	try:
-		page = urlopen(url.encode('utf-8')).read()
-		tagstart = page.find('<title') + 6
-		start = page.find('>', tagstart) + 1
-		end = page.find('</title>')
-		parser = HTMLParser.HTMLParser()
-		return parser.unescape(page[start:end]).replace('"', '')
-	except Exception as error:
-		return '!!!null!!!'
+###############################################################################
+# Database model objects                                                      #
+###############################################################################
 
 class Link(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -92,6 +71,278 @@ class Message(db.Model):
 
 	def __init__(self, last_checked):
 		self.last_checked = last_checked
+
+###############################################################################
+# Interaction methods                                                         #
+###############################################################################
+
+def get_item_by_id(id):
+	item = Link.query.filter_by(id=id).first()
+	return item
+
+def get_item_list():
+	items = Link.query.filter_by(archived = False).all()
+	items = sorted(items, key=lambda link: link.date, reverse=True)
+	return items
+
+def get_archive_list():
+	items = Link.query.filter_by(archived = True).all()
+	items = sorted(items, key=lambda link: link.date, reverse=True)
+	return items
+
+def add_item(url, date, title):
+	new_item = Link(url, date, title)
+	db.session.add(new_item)
+	db.session.commit()
+
+def archive_item(id):
+	item = get_item_by_id(id)
+	item.archived = True
+	item.date = datetime.now()
+	db.session.commit()
+
+def unarchive_item(id):
+	item = get_item_by_id(id)
+	item.archived = False
+	item.date = datetime.now()
+	db.session.commit()
+
+def edit_item_title(id, new_title):
+	item = get_item_by_id(id)
+	item.title = new_title
+	db.session.commit()
+
+def delete_item(id):
+	item = get_item_by_id(id)
+	db.session.delete(item)
+	db.session.commit()
+
+def is_valid_url(url):
+	return urlparse(request.form['url']).hostname is not None
+
+def get_domain(url):
+	return urlparse(url).hostname.replace('www.', '')
+
+def get_title(url):
+	try:
+		page = urlopen(url.encode('utf-8')).read()
+		tagstart = page.find('<title') + 6
+		start = page.find('>', tagstart) + 1
+		end = page.find('</title>')
+		parser = HTMLParser.HTMLParser()
+		return parser.unescape(page[start:end]).replace('"', '')
+	except Exception as error:
+		return '!!!null!!!'
+
+def get_relative_time(date):
+	now = datetime.now()
+	delta = now - date
+	seconds = delta.total_seconds()
+
+	if seconds < 60:
+		return "<1m"
+	elif seconds < (60 * 60):
+		return str(int(seconds / 60)) + "m"
+	elif seconds < (24 * 60 * 60):
+		return str(int(seconds / (60 * 60))) + "h"
+	elif seconds < (7 * 24 * 60 * 60):
+		return str(int(seconds / (24 * 60 * 60))) + "d"
+	elif seconds < (365 * 24 * 60 * 60):
+		return str(int(seconds / (7 * 24 * 60 * 60))) + "w"
+	else:
+		return str(int(seconds / (365 * 24 * 60 * 60))) + "y"
+
+def get_opacity_from_age(date):
+	now = datetime.now()
+	delta = now - date
+	days = delta.days
+
+	if days < 1:
+		return "100"
+	elif days < 3:
+		return "90"
+	elif days < 5:
+		return "80"
+	elif days < 7:
+		return "70"
+	elif days < 14:
+		return "60"
+	elif days < 21:
+		return "50"
+	elif days < 28:
+		return "40"
+	else:
+		return "30"
+
+def delete_if_too_old(item):
+	now = datetime.now()
+	delta = now - item.date
+	days = delta.days
+
+	if days > 7:
+		delete_item(item.id)
+
+###############################################################################
+# Routing methods                                                             #
+###############################################################################
+
+@app.route('/')
+def index():
+	db.create_all()
+
+	items = get_item_list()
+	now = datetime.now()
+
+	opacities = {}
+	times = {}
+	domains = {}
+	positions = {}
+
+	position = len(items);
+
+	for item in items:
+		times[item.id] = get_relative_time(item.date)
+		opacities[item.id] = get_opacity_from_age(item.date)
+		domains[item.id] = get_domain(item.url)
+
+		positions[item.id] = position
+		position -= 1
+
+	return render_template('index.html', title='Link Bucket', emptymessage='No links yet.', items=items, opacities=opacities, times=times, domains=domains, positions=positions)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+	message = ''
+	error = False
+
+	previous_title = ''
+	previous_url = ''
+
+	if request.method == 'POST':
+		url = request.form['url']
+		title = request.form['title']
+		date = datetime.now()
+
+		if '"' in title:
+			message = 'Link titles cannot contain double quote characters.'
+			error = True
+			previous_title = title
+			previous_url = url
+
+		elif not is_valid_url(url):
+			message = 'An invalid URL was given.'
+			error = True
+			previous_title = title
+			previous_url = url
+
+		else:
+			add_item(url, date, title)
+			if title == 'Title':
+				message = "Link added: " + get_domain(url)
+			else:
+				message = "Link added: " + title + " (" + get_domain(url) + ")"
+
+	if len(message) > 0:
+		if error:
+			flash(message, 'error')
+		else:
+			flash(message, 'success')
+
+	return render_template('add.html', title='Link Bucket - Add Link', previous_title=previous_title, previous_url=previous_url)
+
+
+@app.route('/archive')
+def view_archive():
+	items = get_archive_list()
+	items = sorted(items, key=lambda link: link.date, reverse=True)
+	now = datetime.now()
+
+	opacities = {}
+	times = {}
+	domains = {}
+	positions = {}
+	data_edited = False
+	position = len(items)
+
+	for item in items:
+		delete_if_too_old(item)
+
+		times[item.id] = get_relative_time(item.date)
+		opacities[item.id] = 100
+		domains[item.id] = get_domain(item.url)
+
+		positions[item.id] = position
+		position -= 1
+
+	return render_template('archive.html', title='Link Bucket - Archive', emptymessage='The archive is empty.', items=items, opacities=opacities, times=times, domains=domains, positions=positions)
+
+
+@app.route('/archive/<int:id>')
+def archive(id):
+	archive_item(id)
+	return redirect(url_for('index') + "#" + str(request.args.get('scrollto', '')))
+
+@app.route('/unarchive/<int:id>')
+def unarchive(id):
+	unarchive_item(id)
+	return redirect(url_for('view_archive') + "#" + str(request.args.get('scrollto', '')))
+
+@app.route('/edit/<int:id>')
+def edit(id):
+	title = request.args.get('title', '(No title)').replace('"', '')
+	edit_item_title(id, title)
+	return redirect(url_for('index') + "#" + str(request.args.get('scrollto', '')))
+
+@app.route('/title/<path:url>')
+def title(url):
+	return get_title(url)
+
+###############################################################################
+# Facebook routing methods                                                    #
+###############################################################################
+
+@app.route('/fb')
+def fb():
+	details = FB.query.filter_by(id = 1).first()
+	if details is None:
+		details = FB('776255502385741', '3fc41ad7c7cbd4084c341d068c5e02f3')
+		db.session.add(details)
+		db.session.commit()
+	return redirect(facebook_login_url())
+
+@app.route('/fb_logged_in')
+def handle_fb():
+	if 'code' in request.args:
+		details = FB.query.filter_by(id = 1).first()
+		details.code = request.args['code']
+		db.session.commit()
+		return redirect(facebook.get_access_token_from_code(details.code, base_url + url_for('handle_fb'), details.app_id, details.app_secret))
+
+	return '''  <script type="text/javascript">
+				var token = window.location.href.split("access_token=")[1];
+				window.location = "/fb_token/" + token;
+			</script> '''
+
+@app.route('/fb_token/<token>')
+def fb_token(token):
+	pos = token.find('&expires_in=')
+	expires_in = int(token[(pos + 12):])
+	delta = timedelta(seconds=expires_in)
+	expiry = datetime.now() + delta
+
+	details = FB.query.filter_by(id = 1).first()
+	details.access_token = token[:pos]
+	details.expiry = expiry
+	db.session.commit()
+
+	get_facebook_message_links()
+
+	return redirect(url_for('index'))
+
+###############################################################################
+# Facebook methods                                                            #
+###############################################################################
 
 def facebook_login_url():
 	details = FB.query.filter_by(id = 1).first()
@@ -158,14 +409,11 @@ def get_facebook_message_links():
 
 	for link in links:
 		try:
-			new_link = Link(link['url'], link['date'], link['title'])
-			db.session.add(new_link)
+			add_item(link['url'], link['date'], link['title'])
 		except Exception as error:
 			print "An exception occurred: " + str(type(error)) + " - " + str(error)
 			print "The offending link was: \"" + str(link['url']) + "\" with title \"" + str(link['title']) + "\""
 			link_fail_count += 1
-
-	db.session.commit()
 
 	if message_fail_count > 0:
 		unit = " message" if message_fail_count == 1 else " messages"
@@ -212,188 +460,6 @@ def set_last_checked_id(id):
 		details.last_checked = id
 
 	db.session.commit()
-
-@app.route('/')
-def index():
-	db.create_all()
-
-	items = Link.query.filter_by(archived = False).all()
-	items = sorted(items, key=lambda link: link.date, reverse=True)
-	now = datetime.now()
-
-	opacities = {}
-	times = {}
-	domains = {}
-	positions = {}
-
-	position = len(items);
-
-	for item in items:
-		delta = now - item.date
-
-		seconds = delta.total_seconds()
-		times[item.id] = get_relative_time(seconds)
-
-		days = delta.days
-		if days < 1:
-			opacities[item.id] = "100"
-		elif days < 3:
-			opacities[item.id] = "90"
-		elif days < 5:
-			opacities[item.id] = "80"
-		elif days < 7:
-			opacities[item.id] = "70"
-		elif days < 14:
-			opacities[item.id] = "60"
-		elif days < 21:
-			opacities[item.id] = "50"
-		elif days < 28:
-			opacities[item.id] = "40"
-		else:
-			opacities[item.id] = "30"
-
-		domains[item.id] = urlparse(item.url).hostname.replace('www.', '')
-
-		positions[item.id] = position
-		position -= 1
-
-	return render_template('index.html', title='Link Bucket', emptymessage='No links yet.', items=items, opacities=opacities, times=times, domains=domains, positions=positions)
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-	message = ''
-	error = False
-
-	previous_title = ''
-	previous_url = ''
-
-	if request.method == 'POST':
-		if '"' in request.form['title']:
-			message = 'Link titles cannot contain double quote characters.'
-			error = True
-			previous_title = request.form['title']
-			previous_url = request.form['url']
-		elif urlparse(request.form['url']).hostname is None:
-			message = 'An invalid URL was given.'
-			error = True
-			previous_title = request.form['title']
-			previous_url = request.form['url']
-		else:
-			item = Link(request.form['url'], datetime.now(), request.form['title'])
-			db.session.add(item)
-			db.session.commit()
-			showtitle = "" if request.form['title'] == 'Title' else request.form['title']
-			message = "Link added. " + showtitle + " (" + str(urlparse(request.form['url']).hostname.replace('www.', '')) + ")"
-
-	if len(message) > 0:
-		if error:
-			flash(message, 'error')
-		else:
-			flash(message, 'success')
-
-	return render_template('add.html', title='Link Bucket - Add Link', previous_title=previous_title, previous_url=previous_url)
-
-
-@app.route('/archive')
-def view_archive():
-	items = Link.query.filter_by(archived = True).all()
-	items = sorted(items, key=lambda link: link.date, reverse=True)
-	now = datetime.now()
-
-	opacities = {}
-	times = {}
-	domains = {}
-	positions = {}
-	data_edited = False
-	position = len(items)
-
-	for item in items:
-		delta = now - item.date
-
-		days = delta.days
-		if days > 7:
-			db.session.delete(item)
-			data_edited = True
-
-		seconds = delta.total_seconds()
-		times[item.id] = get_relative_time(seconds)
-		opacities[item.id] = 100
-		domains[item.id] = urlparse(item.url).hostname.replace('www.', '')
-
-		positions[item.id] = position
-		position -= 1
-
-	if data_edited:
-		db.session.commit()
-
-	return render_template('archive.html', title='Link Bucket - Archive', emptymessage='The archive is empty.', items=items, opacities=opacities, times=times, domains=domains, positions=positions)
-
-
-@app.route('/archive/<int:id>')
-def archive(id):
-	item = Link.query.filter_by(id=id).first()
-	item.archived = True
-	item.date = datetime.now()
-	db.session.commit()
-	return redirect(url_for('index') + "#" + str(request.args.get('scrollto', '')))
-
-@app.route('/unarchive/<int:id>')
-def unarchive(id):
-	item = Link.query.filter_by(id=id).first()
-	item.archived = False
-	item.date = datetime.now()
-	db.session.commit()
-	return redirect(url_for('view_archive') + "#" + str(request.args.get('scrollto', '')))
-
-@app.route('/edit/<int:id>')
-def edit(id):
-	item = Link.query.filter_by(id=id).first()
-	item.title = request.args.get('title', '(No title)').replace('"', '')
-	db.session.commit()
-	return redirect(url_for('index') + "#" + str(request.args.get('scrollto', '')))
-
-@app.route('/title/<path:url>')
-def title(url):
-	return get_title(url)
-
-
-@app.route('/fb')
-def fb():
-	details = FB('776255502385741', '3fc41ad7c7cbd4084c341d068c5e02f3')
-	db.session.add(details)
-	db.session.commit()
-	return redirect(facebook_login_url())
-
-@app.route('/fb_logged_in')
-def handle_fb():
-	if 'code' in request.args:
-		details = FB.query.filter_by(id = 1).first()
-		details.code = request.args['code']
-		db.session.commit()
-		return redirect(facebook.get_access_token_from_code(details.code, base_url + url_for('handle_fb'), details.app_id, details.app_secret))
-
-	return '''  <script type="text/javascript">
-				var token = window.location.href.split("access_token=")[1];
-				window.location = "/fb_token/" + token;
-			</script> '''
-
-@app.route('/fb_token/<token>')
-def fb_token(token):
-	pos = token.find('&expires_in=')
-	expires_in = int(token[(pos + 12):])
-	delta = timedelta(seconds=expires_in)
-	expiry = datetime.now() + delta
-
-	details = FB.query.filter_by(id = 1).first()
-	details.access_token = token[:pos]
-	details.expiry = expiry
-	db.session.commit()
-
-	get_facebook_message_links()
-
-	return redirect(url_for('index'))
-
 
 if (__name__ == "__main__"):
 	app.run(debug = True)
