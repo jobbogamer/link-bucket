8b66a6fcee39413e8b08afc2d77163acf2949aa6
Josh Asch <josh.asch@gmail.com>
2014-05-15 18:09:33 +0100

    Remove parameters on youtube embed links

--- link-bucket.py
+++ link-bucket.py
@@ -185,7 +185,8 @@ def delete_if_too_old(item):
 def get_youtube_embed_url(url):
 	if "youtube.com" in url:
 		if "watch?v=" in url:
-			return url.replace("watch?v=", "embed/")
+			next_param_pos = url.find("&")
+			return url[:next_param_pos].replace("watch?v=", "embed/")
 
 	return None
 
