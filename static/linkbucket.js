3180d12f4b00cc832bd2702bb2467efc956cab53
Josh Asch <josh.asch@gmail.com>
2014-06-28 12:23:36 +0100

    Display error message when adding a link fails

--- static/linkbucket.js
+++ static/linkbucket.js
@@ -1,4 +1,9 @@
+$(function() {
+	setUpPopovers();
+});
+
 function addLinkFromModal() {
+	hideAddError();
 	var url = document.getElementById('add-modal-url-field').value;
 	var title = document.getElementById('add-modal-title-field').value;
 	$.ajax({
@@ -12,13 +17,30 @@ function addLinkFromModal() {
 		if (success) {
 			$('#add-modal').modal('hide');
 		} else {
-			console.log('oopsie, something went wrong');
+			var error = data['message'];
+			showAddError(error);
 		}
 	});
 }
 
+function hideAddError() {
+	$('#add-modal-error-message').slideUp();
+}
+
 function hideNavbarDropdown() {
 	$('#navbar-collapse').collapse('hide');
+}
+
+function setUpPopovers() {
+	$('#add-modal-error-popover').popover({
+		placement: "bottom",
+		template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+	});
+}
+
+function showAddError(error) {
+	document.getElementById('add-modal-error-popover').dataset.content = error;
+	$('#add-modal-error-message').slideDown();
 }
 
 function toggleCompactMode() {
